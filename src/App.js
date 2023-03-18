a339c2c289b015dcb008b2a19a8b18f785a931d0
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-03-18 21:54:05 +0530

    update login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ToastContainer } from "react-toastify";
 import Header from "./Components/Header/Header";
 import { TailSpin } from "react-loader-spinner";
 import { useGlobalAppContext } from "./Context/AppContext/GlobalContext";
-import React from "react";
+import React, { useEffect } from "react";
 import Circularprogress from "./Components/Loader/Circularprogress";
 import { useGlobalRestApiContext } from "./Context/AppContext/GlobalApiCallContext";
 import { useGlobalAuth } from "./Context/Auth/globalAuthContext";
@@ -13,11 +13,21 @@ import UnProtectedRoute from "./Utils/Router/UnProtectedRoute";
 
 function App() {
   const { loading } = useGlobalRestApiContext();
-  const {updateUser,user} = useGlobalAuth()
+  const { updateUser, user,setUser,checkIfLogedIn } = useGlobalAuth();
+
+
+
+  useEffect(() => {
+    checkIfLogedIn()
+  
+   
+  }, []);
+
+
   return (
     <BrowserRouter>
       <div className="App">
-     { user?  <Router></Router>:<UnProtectedRoute/>}
+        {user ? <Router></Router> : <UnProtectedRoute />}
       </div>
       {loading && <Circularprogress />}
       <ToastContainer></ToastContainer>
